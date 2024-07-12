8dd2b544e9f4a6b6b9941cc88047aab993c464d1
gunasekhar07 <jgsekhar8@gmail.com>
2024-07-12 08:31:35 +0530

    initial commit

---
+++ client/src/utils/validators.js
@@ -0,0 +1,9 @@
+import { isValidUsername } from "6pp";
+
+
+export const usernameValidator = (username) => { 
+  if (isValidUsername(username))
+    return { isvalid:false, errorMessage:' Username is Invalid' };
+
+
+}
